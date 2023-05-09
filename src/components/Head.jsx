67f502a7637497e7e894fbf37a1f76050ed6d465
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-08 21:03:00 -0400

    estilos

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,7 +1,7 @@
 export const Head = () => {
     return (
         <a href="http://projects.terminalkiller.site">
-            <img style={{marginTop:'5rem'}} className="d-block mx-auto mb-2" src="favicon.ico" alt="" width="80" height="80" />
+            <img style={{marginTop:'5rem'}} className="d-block mx-auto mb-2" src="terminalkiller-logo.png" alt="" height="130" />
         </a>
     )
 }
