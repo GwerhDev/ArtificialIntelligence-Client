0b26c27f4e4c0820ed18fc28e7ad6a604dc6729b
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-23 15:27:38 -0400

    background image remove

--- src/app/pages/RemoveBackground.jsx
+++ src/app/pages/RemoveBackground.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { UploadImage } from '../components/UploadImage/UploadImage';
+import { motion } from "framer-motion";
 
 export const RemoveBackground = () => {
   return (
     <main>
-      <div className="px-4 py-2 my-2 text-center border-bottom">
-        <h1 className="display-5 fw-bold">Quitar fondo de imagen</h1>
-        <div className="col-lg-6 mx-auto">
-          <p className="lead mb-0">Sube una imagen y quita el fondo utilizando Tensorflow.js</p>
+      <motion.div initial={{opacity:0}} transition={{duration: 1.5}} animate={{opacity:1}}>
+        <div className="px-4 py-2 my-2 text-center border-bottom">
+          <h1 className="display-5 fw-bold">Quitar fondo de imagen</h1>
+          <div className="col-lg-6 mx-auto">
+            <p className="lead mb-0">Sube una imagen y quita el fondo utilizando Tensorflow.js</p>
+          </div>
         </div>
-      </div>
-      <UploadImage/>
+        <UploadImage/>
+      </motion.div>
     </main>
   )
 }
