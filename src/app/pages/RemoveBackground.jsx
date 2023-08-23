50a6d3d5941294dc6fa4497c84390558641757d8
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-23 15:43:08 -0400

    styles

--- src/app/pages/RemoveBackground.jsx
+++ src/app/pages/RemoveBackground.jsx
@@ -6,7 +6,7 @@ export const RemoveBackground = () => {
   return (
     <main>
       <motion.div initial={{opacity:0}} transition={{duration: 1.5}} animate={{opacity:1}}>
-        <div className="px-4 py-2 my-2 text-center border-bottom">
+        <div className="px-4 py-2 my-2 mb-5 text-center border-bottom">
           <h1 className="display-5 fw-bold">Quitar fondo de imagen</h1>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-0">Sube una imagen y quita el fondo utilizando Tensorflow.js</p>
