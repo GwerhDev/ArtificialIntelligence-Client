c49a314b009c8a61fba9090952e3e5829b5cd0f3
GwerhDev <gwerh.dev@gmail.com>
2024-06-17 11:23:24 -0400

    deleting hashes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import './index.css';
 import React from 'react';
+import App from './app/routes/App';
 import ReactDOM from 'react-dom/client';
 import store from './middlewares/redux/store'
 import { Provider } from 'react-redux';
-import { HashRouter } from "react-router-dom";
-import App from './app/routes/App';
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App/>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
 );
