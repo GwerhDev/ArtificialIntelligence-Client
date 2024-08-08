44c4b3e4b0b5d5ceaac8852fc06b27397a686be1
Gwerh <gwerh.dev@gmail.com>
2024-08-08 14:33:12 -0400

    changing branding

--- src/app/components/Head/Head.jsx
+++ src/app/components/Head/Head.jsx
@@ -1,16 +1,22 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import terminalkillerlogo from "../../../assets/image/png/terminalkiller-logo.png";
+import terminalkillerlogoDesktop from "../../../assets/image/png/terminalkiller-logo.png";
+import terminalkillerlogoMobile from "../../../assets/image/png/terminalkiller-logo-transparent-v.png";
 
 export const Head = () => {
     return (
-        <motion.div initial={{opacity:0}} transition={{duration: 1.5}} animate={{opacity:1}}>
+        <motion.div initial={{ opacity: 0 }} transition={{ duration: 1.5 }} animate={{ opacity: 1 }}>
             <header>
                 <Link to="/">
-                    <img 
-                        className="d-block mx-auto mb-2 mt-5" 
-                        src={terminalkillerlogo}
-                        alt="" height="130" 
+                    <img
+                        className="mx-auto mb-5 mt-5 desktop"
+                        src={terminalkillerlogoDesktop}
+                        alt="" height="150"
+                    />
+                    <img
+                        className="mx-auto mb-5 mt-5 mobile"
+                        src={terminalkillerlogoMobile}
+                        alt="" width="250"
                     />
                 </Link>
             </header>
