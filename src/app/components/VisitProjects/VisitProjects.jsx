965fab56cbc8d368131dff1eba52e55c4da378fe
Gwerh <gwerh.dev@gmail.com>
2025-01-11 00:56:27 -0300

    changing urls

--- src/app/components/VisitProjects/VisitProjects.jsx
+++ src/app/components/VisitProjects/VisitProjects.jsx
@@ -2,7 +2,7 @@ import s from "./VisitProjects.module.css"
 
 export const VisitProjects = () => {
     return (
-        <a href="http://projects-terminalkiller.netlify.app/">
+        <a href="http://projects.terminalkiller.cl/">
             <button className={s.buttonVisit}>Visitar TerminalKiller Project</button>
         </a>
     )
